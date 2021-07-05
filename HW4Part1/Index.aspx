85812020d5c2bacbbd5148a071ab57b449d7bb52
Albert De La Cruz <adelac10@asu.edu>
2021-07-05 15:39:27 -0700

    HW4pt1, an aspx page that displays a restaurant type of xml document included with the project

---
+++ HW4Part1/Index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="HW4Part1.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            XML URL<br />
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" Width="566px">https://www.public.asu.edu/~adelac10/Restaurants.xml</asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+            <br />
+            <div id="BigDiv" runat="server">
+            </div>
+        </div>
+        <div ID="TheDiv" runat="server">
+
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ HW4Part1/Index.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace HW4Part1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected XmlDocument doc = new XmlDocument();
+        protected XmlNode node;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string file = TextBox1.Text;
+            XPathDocument dx = new XPathDocument(file);
+            XPathNavigator nav = dx.CreateNavigator();
+            XPathNodeIterator iterator = nav.Select("/Restaurants/Restaurant");//goes through each restaurant
+            while(iterator.MoveNext())
+            {
+                XPathNodeIterator it = iterator.Current.Select("Name");
+                it.MoveNext();
+                TheDiv.InnerHtml = TheDiv.InnerHtml + "<br /> <br />Restaurant Name: " + it.Current.Value;//sets name
+                it = iterator.Current.Select("Contact/Phone");
+                it.MoveNext();
+                TheDiv.InnerHtml = TheDiv.InnerHtml + "<br />Contact Information:<br />&nbsp;&nbsp;Phone: " + it.Current.Value;//sets phone
+                it = iterator.Current.Select("Contact/Email");
+                it.MoveNext();
+                TheDiv.InnerHtml = TheDiv.InnerHtml + "<br />&nbsp;&nbsp;Email: " + it.Current.Value;//sets Email
+                it = iterator.Current.Select("@Delivery");
+                it.MoveNext();
+                TheDiv.InnerHtml = TheDiv.InnerHtml + "<br />Delivery: " + it.Current.Value;//sets Email
+                it = iterator.Current.Select("Website");
+                it.MoveNext();
+                TheDiv.InnerHtml = TheDiv.InnerHtml + 
+                    "<br />Website: <a href='" + it.Current.Value + "'>" + it.Current.Value+"</a>";//sets websites
+            
+                if (it.Current.HasAttributes)
+                {
+                    it = iterator.Current.Select("Website/@facebook");
+                    it.MoveNext();
+                    TheDiv.InnerHtml = TheDiv.InnerHtml + "<br />Facebook: " + it.Current.Value;//sets Email
+                }
+            }
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
